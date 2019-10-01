c173df944b3a19f31e926cb484b96ce3f9ea8ebc
artemoneto <47323236+artemoneto@users.noreply.github.com>
2019-10-01 16:16:22 +0300

    Update foo.sh

--- foo.sh
+++ foo.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-#This script requires sudo to work properly
-
-#Downloading and installing Go lang necessary for building IPFS
-snap install go --classic 	#requires sudo
+#Downloading and isntalling Go lang necessary for building IPFS
+sudo snap install go --classic 	#requires sudo
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:$GOPATH/bin
 source $HOME/.profile
 
-#installing git
-apt install git 		#requires sudo
+#isntalling git
+sudo apt install git 		#requires sudo
 
 #Downloading and building IPFS
 git clone https://github.com/ipfs/go-ipfs.git
